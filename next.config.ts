963af30bb0485cd3e968ae8e5ebb829cb6f0c9c5
Miguel Roncagliolo <69696772+MiguelRoncagliolo@users.noreply.github.com>
2026-06-12 23:33:21 -0400

    Trace remaining Tesseract runtime dependencies

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,12 @@ const nextConfig: NextConfig = {
     "/*": [
       "./node_modules/tesseract.js/**/*",
       "./node_modules/tesseract.js-core/**/*",
+      "./node_modules/bmp-js/**/*",
+      "./node_modules/idb-keyval/**/*",
       "./node_modules/regenerator-runtime/**/*",
       "./node_modules/is-url/**/*",
       "./node_modules/node-fetch/**/*",
+      "./node_modules/wasm-feature-detect/**/*",
       "./node_modules/zlibjs/**/*",
     ],
   },
